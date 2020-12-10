3b2b17efaf16e152a32bbe56baaf68570116a15d
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2020-12-11 05:53:51 +0800

    timer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var taskDetails = [
-        {id: 0, name: "Fix Start and End Timer", hours: "00", mins: "00", seconds: "00"},
-        {id: 1, name: "Filterning", hours: "00", mins: "00", seconds: "00"}
+        {id: 0, name: "Fix Start and End Timer", startTimeHours: null, startTimeMins: null, startTimeSeconds: null, endTimeHours: null, endTimeMins: null, endTimeSeconds: null, duration: null},
+        {id: 1, name: "Filterning", startTimeHours: null, startTimeMins: null, startTimeSeconds: null, endTimeHours: null, endTimeMins: null, endTimeSeconds: null, duration: null}
     ]
     var count = 2;
     // Creatiing new task, added task appends to list
@@ -9,9 +9,9 @@ $(document).ready(function(){
         var newTaskTitle = document.getElementById("new-task-title").value
         if(newTaskTitle != ''){
             console.log(newTaskTitle)
-            $("ul").prepend('<li><div class="card"><div class="card-body"><div class="row"><div class="col text-style"><p class="p-margin">' + newTaskTitle + '</p></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch"></span></div><div class="row"><div class="col"><button type="button" class="btn btn-secondary start-style" id="start">Start</button></div><div class="col"><button type="button" class="btn btn-secondary end-style" id="end">End</button></div></div></div></div></div></div></li>'); 
+            $("ul").prepend('<li id="task-card"><div class="card"><div class="card-body"><div class="row"><div class="col text-style"><span class="p-margin">' + newTaskTitle + '</span></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch"></span></div><div class="row"><div class="col"><button type="button" class="btn btn-secondary start-style" id="start">Start</button></div><div class="col"><button type="button" class="btn btn-secondary end-style" id="end">End</button></div></div></div></div></div></div></li>'); 
             document.getElementById('new-task-title').value = ''
-            taskDetails.push({id: count++, name: newTaskTitle, hours: "00", mins: "00", seconds: "00"})
+            taskDetails.push({id: count++, name: newTaskTitle, startTimeHours: null, startTimeMins: null, startTimeSeconds: null, endTimeHours: null, endTimeMins: null, endTimeSeconds: null, duration: null})
             console.log(taskDetails)
         } else {
             alert("No Task!")
@@ -42,65 +42,65 @@ $(document).ready(function(){
         console.log(taskDetails)
         $('ul').empty()
         for(i = 0; i<taskDetails.length; i++){
-            $("ul").append('<li><div class="card"><div class="card-body"><div class="row"><div class="col text-style"><p class="p-margin">' + taskDetails[i].name + '</p></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch"></span></div><div class="row"><div class="col"><button type="button" class="btn btn-secondary start-style" id="start">Start</button></div><div class="col"><button type="button" class="btn btn-secondary end-style" id="end">End</button></div></div></div></div></div></div></li>'); 
+            $("ul").append('<li id="task-card"><div class="card"><div class="card-body"><div class="row"><div class="col text-style"><span class="p-margin">' + taskDetails[i].name + '</span></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch"></span></div><div class="row"><div class="col"><button type="button" class="btn btn-secondary start-style" id="start">Start</button></div><div class="col"><button type="button" class="btn btn-secondary end-style" id="end">End</button></div></div></div></div></div></div></li>'); 
         }
     });
 
-    /**  STOPWATCH **/
-    var Stopwatch;
-    if (!Stopwatch) 
-        Stopwatch = {};
-
-    function Stopwatch(displayTime){
-        this.runtime = 0;
-        this.timer = null; 
-        this.displayTime = displayTime;
-    }
-
-    Stopwatch.INCREMENT = 200
-
-    // Display Time
-    Stopwatch.prototype.doDisplay = function(){
-        if (!this.laptime) 
-            this.displayTime(this.runtime);
-        else 
-            this.displayTime(this.laptime);
-    }
-
-    // Time Start Function
-    Stopwatch.prototype.start = function(){
-        var instance = this;
-        this.timer = window.setInterval(function(){
-            instance.runtime += Stopwatch.INCREMENT;
-            instance.doDisplay();
-        }, Stopwatch.INCREMENT);
-    }
-
-    // Time Stop Function
-    Stopwatch.prototype.stop = function(){
-        window.clearInterval(this.timer);
-        this.timer = null;
-        this.doDisplay();
-    }
-
-    // Time Format (H:M:S)
-    var stopwatchFormat = new Stopwatch(function(runtime) {
-        var hours = Math.floor(runtime / 3600000);
-        var minutes = Math.floor(runtime / 60000);
-        var seconds = Math.floor(runtime % 60000 / 1000);
-        var displayText =  hours + ":" +  minutes + ":" + (seconds < 10 ? "0" : "") + seconds ;
-        $("#stopwatch").text(displayText);
+    // Start Time
+    $(document).on('click', ' #start', function() {
+        console.log(this)
+        console.log($(this).closest('li').prop('id'));
+        console.log($(this).closest('li').find('.p-margin').text())
+        var taskName = $(this).closest('li').find('.p-margin').text()
+        var now = new Date(Date.now());
+        var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        console.log(formatted)
+        // $("#stopwatch").replaceWith("Start Time: " + formatted)
+        for(i = 0; i < taskDetails.length; i++) {
+            if(taskName == taskDetails[i].name){
+                taskDetails[i].startTimeHours = now.getHours()
+                taskDetails[i].startTimeMins = now.getMinutes()
+                taskDetails[i].startTimeSeconds = now.getSeconds()
+            }
+        }
+        console.log(taskDetails)
+        // $(this).closest('li').toggleClass('strike').fadeOut('slow', function() { 
+            // stopwatchFormat.start();
+        // });
     });
-
-    // Start Stopwatch
-    $('ul').on('click', '#start', function(){
-        console.log("start")
-        stopwatchFormat.start();
-    });
-
+    
     // End Stopwatch
-    $("ul").on("click", "#end", function(){
-        console.log("end")
-        stopwatchFormat.stop(); 
+    $(document).on('click', ' #end', function() {
+        console.log(this)
+        console.log($(this).closest('li').prop('id'));
+        console.log($(this).closest('li').find('.p-margin').text())
+        var taskName = $(this).closest('li').find('.p-margin').text()
+        var now = new Date(Date.now());
+        var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        console.log(formatted)
+
+        for(i = 0; i < taskDetails.length; i++) {
+            if(taskDetails[i].startTimeHours != null){
+                if(taskName == taskDetails[i].name){
+                    taskDetails[i].endTimeHours = now.getHours()
+                    taskDetails[i].endTimeMins = now.getMinutes()
+                    taskDetails[i].endTimeSeconds = now.getSeconds()
+
+                    var hours = taskDetails[i].endTimeHours - taskDetails[i].startTimeHours
+                    var mins = taskDetails[i].endTimeMins - taskDetails[i].startTimeMins
+                    var seconds = taskDetails[i].endTimeSeconds - taskDetails[i].startTimeSeconds
+                
+                    var computation = hours + ":" + mins + ":" + seconds 
+                    console.log(computation)
+
+                    taskDetails[i].duration = computation
+                    $("#stopwatch").replaceWith("( " + computation + " )")
+                }
+            }
+        }
+        console.log(taskDetails)
+        // $(this).closest('li').toggleClass('strike').fadeOut('slow', function() { 
+            // stopwatchFormat.start();
+        // });
     });
 });
