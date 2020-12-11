ded6856d246bb008eab8026991f4b4dfda39bb3a
michikogo <37466954+michikogo@users.noreply.github.com>
2020-12-11 18:09:59 +0800

    inital edit task

--- index.js
+++ index.js
@@ -18,12 +18,14 @@ $(document).ready(function(){
     $.each(taskDetails, function(index, value){
         console.log(taskDetails)
         durationSplit = value.duration.split(':')
-        $("ul").append('<li id="task-card"><div class="card"><div class="card-body"><div class="row">'
-                        + '<div class="row"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-square close-style" id="delete-task" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
+        $("ul").append('<li id="task-card"><div class="card"><div class="card-body"><div class="row"><div class="row">' 
+                        + '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil edit-style" fill="currentColor" id="edit-task" xmlns="http://www.w3.org/2000/svg">'
+                        + '<path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>'
+                        + '</svg><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-square close-style" id="delete-task" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
                         + '<path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>'
                         + '<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'
                         + '</svg></div><div class="col text-style"><span class="p-margin">' 
-                        + value.name + '</span></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch">(' 
+                        +  value.name + '</span></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch">(' 
                         + durationSplit[0] + ':' + durationSplit[1] + ':' + durationSplit[2] + ')</span></div>'
                         + '<div class="row"><div class="col"><button type="button" class="btn btn-secondary start-style" id="start">Start</button>'
                         + '</div><div class="col"><button type="button" class="btn btn-secondary end-style" id="end" disabled="disabled">End</button>'
@@ -36,12 +38,14 @@ $(document).ready(function(){
         var newTaskTitle = document.getElementById("new-task-title").value
         if(newTaskTitle != ''){
             console.log("[NEW TASK] newTaskTitle: " + newTaskTitle)
-            $("ul").prepend('<li id="task-card"><div class="card"><div class="card-body"><div class="row">'
-                                + '<div class="row"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-square close-style" id="delete-task" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
+            $("ul").prepend('<li id="task-card"><div class="card"><div class="card-body"><div class="row"><div class="row">' 
+                                + '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil edit-style" fill="currentColor" id="edit-task" xmlns="http://www.w3.org/2000/svg">'
+                                + '<path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>'
+                                + '</svg><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-square close-style" id="delete-task" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
                                 + '<path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>'
                                 + '<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'
                                 + '</svg></div><div class="col text-style"><span class="p-margin">' 
-                                +  newTaskTitle + '</span></div><div class="col-5"><div class="row">'
+                                + newTaskTitle + '</span></div><div class="col-5"><div class="row">'
                                 + '<span class="stopwatch-style" id="stopwatch">(00:00:00)</span></div><div class="row"><div class="col">' 
                                 + '<button type="button" class="btn btn-secondary start-style" id="start">Start</button></div><div class="col">'
                                 + '<button type="button" class="btn btn-secondary end-style" id="end" disabled="disabled">End</button></div></div></div></div></div></div></li>'
@@ -82,12 +86,14 @@ $(document).ready(function(){
         $('ul').empty()
         for(i = 0; i<taskDetails.length; i++){
             durationSplit = taskDetails[i].duration.split(':')
-            $("ul").append('<li id="task-card"><div class="card"><div class="card-body"><div class="row">'
-                            + '<div class="row"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-square close-style" id="delete-task" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
+            $("ul").append('<li id="task-card"><div class="card"><div class="card-body"><div class="row"><div class="row">' 
+                            + '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil edit-style" fill="currentColor" id="edit-task" xmlns="http://www.w3.org/2000/svg">'
+                            + '<path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>'
+                            + '</svg><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-square close-style" id="delete-task" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
                             + '<path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>'
                             + '<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'
                             + '</svg></div><div class="col text-style"><span class="p-margin">' 
-                            +  taskDetails[i].name + '</span></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch">(' 
+                            + taskDetails[i].name + '</span></div><div class="col-5"><div class="row"><span class="stopwatch-style" id="stopwatch">(' 
                             + durationSplit[0] + ':' + durationSplit[1] + ':' + durationSplit[2] + ')</span></div>'
                             + '<div class="row"><div class="col"><button type="button" class="btn btn-secondary start-style" id="start">Start</button>'
                             + '</div><div class="col"><button type="button" class="btn btn-secondary end-style" id="end" disabled="disabled">End</button>'
@@ -104,7 +110,7 @@ $(document).ready(function(){
         var newTotalDuration = 0
 
         var taskName = $(this).closest("li").find('.p-margin').text()
-        console.log(taskName)
+        // console.log(taskName)
 
         // Removed
         $(this).closest('li').remove();
@@ -130,6 +136,13 @@ $(document).ready(function(){
         console.log(taskDetails)
     });
     
+    // Editing
+    $(document).on('click', '#edit-task', function(){
+        var taskName = $(this).closest("li").find('.p-margin').text()
+        console.log(taskName)
+        $('.p-margin').val()
+    })
+
     // Start Time
     $(document).on('click', ' #start', function() {
         // console.log(this)
